2176a1334ec361e57bc9545c94e7fd7d22d7934b
id2k1149 <id2k1149@gmail.com>
2023-04-13 09:01:29 -0400

    Initial Commit

---
+++ SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Max Franz Immelmann on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
